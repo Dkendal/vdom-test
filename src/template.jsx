70453723fd95b2081067b8b7b2c7e2192dbc437f
Dylan C. Kendal <dylan.kendal@shopify.com>
2019-05-10 10:23:23 -0400

    Delete template.jsx

--- src/template.jsx
+++
@@ -1,9 +0,0 @@
-export function template() {
-    function handleClick(e) { }
-    return (<div>
-      <div>sup</div>
-      <div>
-        <button onClick={handleClick}/>
-      </div>
-    </div>);
-}
